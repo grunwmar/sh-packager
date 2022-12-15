fb5627435fbeec5f9e188fbe32583e7574cf4710
Martin Grunwald <martingrunwald91@gmail.com>
2022-12-15 22:51:00 +0100

    Added ability to tell the script containing package where to extract its content using --path /some/path argument. Removed colors. Support of exclude files. Gzip changed to XZ.

--- pack.sh
+++ pack.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
-WD="$PWD"
-DIRNAME=$(basename "$WD")
-SHDIR=$(dirname "$0")
+WD="$PWD" # location project directory
+DIRNAME=$(basename "$WD") # name of project directory
+SHDIR=$(dirname ${BASH_SOURCE[0]}) # location of packager
 
-echo $DIRNAME
+echo "** Packing '"$WD"'."
 cd ..
-echo $PWD
 
 SHNAME="$DIRNAME".sh
 B64NAME="$DIRNAME".b64
 
-TMP_TARPATH="$PWD"/"tmp_$DIRNAME".tar.gz
-TMP_B64PATH="$PWD"/"tmp_$DIRNAME".b64
+TMP_TARPATH="$PWD"/"tmp_$DIRNAME".tarxz
+TMP_B64PATH="$PWD"/"tmp_$DIRNAME".tarxz64
+EXCLUDEFILE_PREDEF="$SHDIR"/"exclude.txt"
+EXCLUDEFILE="$WD"/"exclude.txt"
 
-tar czf "$TMP_TARPATH" --exclude="venv" --exclude="env" --exclude="pyenv" --exclude="python" --exclude="__pycache__" --exclude=".git" -C $PWD "$DIRNAME"
+if [ -f "$EXCLUDEFILE" ]; then
+    echo "Using exclude file $EXCLUDEFILE"
+    tar cJf "$TMP_TARPATH" --exclude-from="$EXCLUDEFILE_PREDEF" --exclude-from="$EXCLUDEFILE" -C $PWD "$DIRNAME"
+else
+    tar cJf "$TMP_TARPATH" --exclude-from="$EXCLUDEFILE_PREDEF" -C $PWD "$DIRNAME"
+fi
+
 cat "$TMP_TARPATH" | base64 > "$TMP_B64PATH"
 
 export APPPACK_WD="$WD"
@@ -22,5 +29,12 @@ export APPPACK_DIRNAME="$DIRNAME"
 export APPPACK_B64PATH="$TMP_B64PATH"
 
 rm "$TMP_TARPATH"
-python3 "$SHDIR"/make_sh.py
+python3 "$SHDIR/make_sh.py"
+chmod +x "$WD/$DIRNAME.pkgsh"
 rm "$TMP_B64PATH"
+
+export APPPACK_WD=""
+export APPPACK_DIRNAME=""
+export APPPACK_B64PATH=""
+echo "Directory '$DIRNAME' packed as '$WD/$DIRNAME.pkgsh'"
+echo "Done."
